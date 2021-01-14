b65bfbc81630e31c9feac6c45e701df25eace544
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-14 10:55:52 +0530

    Create BinarySearch.java

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,53 @@
+// array should be sorted
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int[]  arr = input();
+	    display(arr);
+	    int elementToSearch = sc.nextInt();
+	    binarySearch(arr,elementToSearch);
+        
+	   
+	}
+	
+	public static int[] input() {
+	    int n = sc.nextInt();
+	    int[] arr = new int[n];
+	    for(int i=0;i<n;i++) {
+	        arr[i] = sc.nextInt();
+	    }
+	    return arr;
+	}
+	public static void display(int[] arr) {
+	    for(int val:arr) {
+	        System.out.println(val);
+	    }
+	}
+	public static void binarySearch(int[] arr,int elementToSearch) {
+	    int low = 0;
+	    int high = arr.length-1;
+	    
+	    while(low<=high) {
+	        int mid = (low+high)/2;
+	        if(arr[mid]>elementToSearch) {
+	            high = mid-1;
+	        }
+	        else if(arr[mid]<elementToSearch) {
+	            low = mid+1;
+	        }
+	        else {
+	            System.out.println(arr[mid]);
+	            break;
+	        }
+	    }
+	}
+}
