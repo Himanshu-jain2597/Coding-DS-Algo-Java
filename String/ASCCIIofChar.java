60510f9116f83057bc198861ff90ec4caac58980
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-16 11:47:05 +0530

    Create ASCCIIofChar.java

---
+++ String/ASCCIIofChar.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	     char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+	   
+
+	    
+	
+}
