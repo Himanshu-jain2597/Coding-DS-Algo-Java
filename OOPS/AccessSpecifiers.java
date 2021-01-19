cfe4e407ab63481bd2115c3f086ab3985a05c262
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-19 13:13:15 +0530

    Create AccessSpecifiers.java

---
+++ OOPS/AccessSpecifiers.java
@@ -0,0 +1,10 @@
+// there are four access specifiers in java :- private,protected, public ,default.. 
+
+
+// private:- visible inside same class..
+// public:- public everywhere.
+// protected:- 
+// default :- visible inside same package.  
+
+
+// getter and setter function are used to operate in the private member of the class..
