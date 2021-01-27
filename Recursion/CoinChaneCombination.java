431c71c85b36ccf55de8348be84189c0d6fad7bd
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-27 17:54:37 +0530

    Create CoinChaneCombination.java

---
+++ Recursion/CoinChaneCombination.java
@@ -0,0 +1,37 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    static int count =0;
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int [] demos = {2,3,5,6};
+	    coinChange(demos,10,"",0);
+	    
+		// your code goes here
+	}
+	
+	public static void coinChange(int[] demos,int amount, String ans,int lastindex) {
+	    
+	    if(amount==0) {
+	        System.out.println(ans);
+	        return;
+	        
+	    }
+	    
+	    for(int i=lastindex;i<demos.length;i++) {
+	        if(amount>=demos[i]) {
+	        
+	        coinChange(demos,amount-demos[i],ans+demos[i],i);
+	        }
+	        
+	    }
+	    
+
+	}
+}
