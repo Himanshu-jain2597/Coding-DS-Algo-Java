a53cd387fe8074ad3ddd2a67a25b93e61e9afc68
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-02-14 17:41:42 +0530

    Create Theory.java

---
+++ OOPS/Theory.java
@@ -0,0 +1,7 @@
+// final variable can be initalised ony once 
+
+
+// static vatiable do not create copy for each object. it is class variable.  it is refer by class name for ex:- student.noOfStudent
+
+
+// non static function can access both static and non static variable 
