b911895ee21b82c30b0e6fd29e18761112b2f769
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-02-20 16:35:39 +0530

    Create MissingNumber.java

---
+++ Bit-magic/MissingNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    int MissingNumber(int array[], int n) {
+        
+        
+        int x1 = 0; 
+        for(int i=0;i<array.length;i++) {
+            x1^= array[i];
+        }
+        
+        int x2 = 0;
+        for(int i=1;i<=n;i++) {
+            x2^= i;
+        }
+        
+        return x1^x2;
+        
+        
+        // Your Code Here
+    }
+}
