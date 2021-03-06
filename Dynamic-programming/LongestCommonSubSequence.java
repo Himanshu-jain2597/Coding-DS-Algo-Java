ccf73408591ec7649d6c21590d406e89845805c0
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-03-06 16:46:27 +0530

    Create LongestCommonSubSequence.java

---
+++ Dynamic-programming/LongestCommonSubSequence.java
@@ -0,0 +1,39 @@
+int lcs(int x, int y, string s1, string s2,int[][] str){
+    
+    if(text1.length()==0||text2.length()==0) {
+            return 0;
+        }
+        
+        if(str[x][y]!=0) {
+            return str[x][y];
+        }
+        
+        int ans =0;
+        
+       if(text1.charAt(0)== text2.charAt(0)) {
+            
+          
+            
+            ans =  1 + longestCommonSubsequence(x+1,y+1,ros1, ros2);
+            
+            
+        }
+        else {
+            
+            int first = longestCommonSubsequence(x,y+1,ros1, text2);
+            int second = longestCommonSubsequence(x+1,y,text1, ros2);
+            
+            ans =  Math.max(first,second);
+            
+            
+            
+        }
+        
+        str[x][y] = ans;
+        return ans;
+        
+        
+    
+     
+    
+}
